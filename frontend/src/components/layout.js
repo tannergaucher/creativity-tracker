2a64b3dc07f8dca278000e129a8abdcb0469529f
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-02-28 12:06:23 +0800

    Add loading spinner styled component

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -1,9 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
 import { createGlobalStyle, ThemeProvider } from "styled-components"
-
 import { Grommet } from "grommet"
 
 import Bar from "../components/Bar"
